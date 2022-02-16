ef7f0de5a04f7b2c4445eb0a591636a15504b88c
Vaiolo <91316641+Vaiolo@users.noreply.github.com>
2022-02-16 13:10:26 +0100

    Registration/Mailer

---
+++ PHPMailer/mail.php
@@ -0,0 +1,48 @@
+<?php
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+
+        require 'src/Exception.php';
+        require 'src/PHPMailer.php';
+        require 'src/SMTP.php';
+
+
+       if(isset($_POST['submit'])) {
+
+
+               $mail = new PHPMailer;
+               $mail->isSMTP();
+                               $mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+               $mail->SMTPDebug = 0; //change to 0 to stop showing u debug messages
+               $mail->Host = "smtp.gmail.com";
+               $mail->Port = "587"; // typically 587
+               $mail->SMTPSecure = 'tls'; // ssl is depracated
+               $mail->SMTPAuth = true;
+               $mail->Username = "groupabooking@gmail.com";
+               $mail->Password = "Toucangiant2021";
+               $mail->setFrom("groupabooking@gmail.com", "Fake Amazon");
+               $mail->addAddress($_POST['email']);
+               $mail->Subject = 'Your registration was successful!';
+               $mail->msgHTML("Dear" . $_POST['first_name'] . ",
+
+                Thank you for completing your registration with us.
+
+                This email serves as a confirmation that your account is activated and that you are officially a part of the family.
+                Enjoy!
+
+                Regards,
+                The team"); // remove if you do not want to send HTML email
+               $mail->AltBody = 'HTML not supported';
+
+               $mail->send();
+
+
+       }
+
+?>
--- Register/registration.php
+++ Register/registration.php
@@ -38,7 +38,7 @@
                                                     $query = "INSERT INTO user (Firstname, Lastname, City, Address, Email, Country, DateofBirth, Password, AccountType) VALUES (?,?,?,?,?,?,?,?,?)";
 
                                                       if($statement = mysqli_prepare($conn, $query)){
-                                                          mysqli_stmt_bind_param($statement, 'ssssssdsi', $first_name, $last_name, $city, $address, $email, $country, $birth, $password, $accountType);
+                                                          mysqli_stmt_bind_param($statement, 'ssssssssi', $first_name, $last_name, $city, $address, $email, $country, $birth, $password, $accountType);
                                                           if(mysqli_stmt_execute($statement)){
                                                               echo "Registration Confirmed";
                                                             }else{
@@ -48,12 +48,14 @@
                                                           } else {
                                                             echo "error: " . $query . "<br>" . mysqli_error($conn);
                                                           }
+                                                            include("../Phpmailer/mail.php");
                                                     }
                                                 }
                                                 else {
                                                     echo "Error executing query!<br>";
                                                     die(mysqli_error($conn));}
                                             }
+
 
                                   }  else {echo "Please fill in your password!";}
                              }  else {echo "Please fill in your email!";}
@@ -65,3 +67,61 @@
     }  else {echo "Please fill in your first name!";}
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="../Home/style.css">
+<body>
+
+
+</body>
+<div class="container_login">
+    <?php include("../header and footer/header.php")?>
+    <div class="description">
+        <h2 id="title">REGISTER</h2>
+    </div>
+
+
+    <div id="registration">
+        <div class="registration_box">
+          <form method="post" action="registration.php">
+
+              <label for="firstName">First Name:</label><br>
+              <input type="text" name="first_name" id="firstName"><br>
+
+              <label for="lastName">Last Name:</label><br>
+              <input type="text" name="last_name" id="lastName"><br>
+
+              <label for="Address">Address:</label><br>
+              <input type="text" name="address" id="Address"><br>
+
+               <label for="city">City:</label><br>
+              <input type="text" name="city" id="city"><br>
+
+               <label for="country">Country:</label><br>
+              <input type="text" name="country" id="country"><br>
+
+              <label for="birth">Date of birth:</label><br>
+              <input type="date" name="birth" /><br>
+
+              <label for="emailAddress">Email Address:</label><br>
+              <input type="text" name="email" id="emailAddress"><br>
+
+              <label for="password">Password:</label><br>
+              <input type="password" name="password" id="password"><br>
+
+          <input type="submit" name="submit" value="Submit" id="registeraccount">
+      </form>
+  </div>
+</div>
+
+    <?php
+      include('../header and footer/footer.php');
+    ?>
+</div>
+</head>
+</html>
