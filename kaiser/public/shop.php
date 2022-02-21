b052459092914a6f09c7c466885666eeae2dd933
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-02-21 22:52:44 +0100

    Added my files

---
+++ kaiser/public/shop.php
@@ -0,0 +1,28 @@
+<?php require_once ("../resources/config.php");?>
+<?php include ("../resources/templates/front/header.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Jumbotron Header -->
+        <header>
+                <h1>Shop</h1>
+        </header>
+
+        <hr>
+        <!-- /.row -->
+
+        <!-- Page Features -->
+        <div class="row text-center">
+
+            <?php get_products_in_shop_page();?>
+
+
+        <!-- /.row -->
+
+
+
+    </div>
+    <!-- /.container -->
+        <?php include ("../resources/templates/front/footer.php"); ?>
+
