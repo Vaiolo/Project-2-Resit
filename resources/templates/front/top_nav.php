0f9f32a2603c31ca6050ce3fa4087b8be2d20895
kaiseraftab <91316936+kaiseraftab@users.noreply.github.com>
2022-02-25 02:51:07 +0100

    Add files via upload

---
+++ resources/templates/front/top_nav.php
@@ -0,0 +1,19 @@
+<header>
+        <div class="left_header">
+            <h3 class="header_logo"><a href="index.php">NHL WEBSHOP</a></h3>
+        </div>
+        <div class="right_header">
+            <ul>
+                <li class="li_header"><a href="shop.php">Home</a></li>
+                <li class="li_header"><a href="login.php">Login</a></li>
+                <li class="li_header"><a href="registration.php">Registration</a></li>
+                <li class="li_header"><a href="#">Orders</a></li>
+                <li class="li_header"><a href="#">Shopping</a></li>
+                <li class="li_header"><a href="#">Product</a></li>
+                <li class="li_header"><a href="contact.php">Contact us</a></li>
+            </ul>
+        </div>
+        <div class="search">
+            <input class="header_search" type="text" name="search" placeholder="Search here"><input class="header_submit" type="submit" name="submit" value="Search">
+        </div>
+</header>
