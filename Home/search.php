aa4b932df4f660bc297c89ce6f26cfd8a1b6a375
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-03-04 02:59:04 +0100

    Update search.php

--- Home/search.php
+++ Home/search.php
@@ -1,28 +1,15 @@
 <?php require_once ("../resources/config.php");?>
 <?php include('../header and footer/header.php');?>
-
-
-
-
 <div class="container">
-
     <div class="row">
-
         <?php include ("../resources/templates/front/side_nav.php"); ?>
-
         <div class="col-md-9">
-
             <div class="row">
-
                 <?php search(); ?>
-            </div><!--row ends here -->
-
+            </div>
         </div>
-
     </div>
-
 </div>
-<!-- /.container -->
 <?php include('../header and footer/footer.php');?>
 
 
