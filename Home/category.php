e8533b4f48658d56f44cab1bda57cb52fc6bf1e7
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-03-04 02:53:29 +0100

    Update category.php

--- Home/category.php
+++ Home/category.php
@@ -1,33 +1,14 @@
 <?php require_once ("../resources/config.php");?>
 <?php include('../header and footer/header.php');?>
-
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Jumbotron Header -->
-
-
-        <hr>
-
-        <!-- Title -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h3>Latest Products</h3>
-            </div>
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="col-lg-12">
+            <h3>Latest Products</h3>
         </div>
-        <!-- /.row -->
-
-        <!-- Page Features -->
-        <div class="row text-center">
-
-            <?php get_products_in_cat_page();?>
-
-
-        <!-- /.row -->
-
-
-
     </div>
-    <!-- /.container -->
-        <?php include '../header and footer/footer.php';?>
+    <div class="row text-center">
+        <?php get_products_in_cat_page();?>
+    </div>
+    <?php include '../header and footer/footer.php';?>
 
