51bc2bbeff7f3e8e0c8ed5b35821f49738f1264d
Kaiseraftab <kaiser.aftab@student.nhlstenden.com>
2022-03-04 10:41:25 +0100

    Update delete_product.php

--- resources/templates/back/delete_product.php
+++ resources/templates/back/delete_product.php
@@ -1,7 +1,7 @@
 <?php require_once ("../../config.php");
 
 if(isset($_GET['id'])){
-    $query = query(" DELETE FROM products WHERE product_id = " . escape_string($_GET['id']). " ");
+    $query = query(" DELETE FROM product WHERE ProductId = " . escape_string($_GET['id']). " ");
     confirm($query);
 
     set_message("Product Deleted");
